253663e8f353daefb373ebbfdf9a39283aa9d73b
irvanzarkasie <irvanzarkasie@gmail.com>
2019-12-22 14:33:06 +0700

    Set output data into new field

--- HiveReadWritePipeline/ReadPipeline.py
+++ HiveReadWritePipeline/ReadPipeline.py
@@ -7,6 +7,6 @@ logger = LoggerWrapper.init_logger(__name__)
 def read_from_table(**kwargs):
   logger.debug("Read data from Hive with table name: " + kwargs["input"]["target"])
   sqlcontext = PysparkWrapper.init_processor(kwargs["id"])
-  kwargs["output"] = sqlcontext.sql("select * from " + kwargs["input"]["target"])
+  kwargs["output"]["data"] = sqlcontext.sql("select * from " + kwargs["input"]["target"])
   return kwargs
 # end def
