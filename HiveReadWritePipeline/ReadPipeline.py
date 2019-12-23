fb07f06ca7e251af1ab7feabba0f72f1ec269416
irvanzarkasie <irvanzarkasie@gmail.com>
2019-12-23 14:25:36 +0700

    Introduce new filler

--- HiveReadWritePipeline/ReadPipeline.py
+++ HiveReadWritePipeline/ReadPipeline.py
@@ -7,6 +7,7 @@ logger = LoggerWrapper.init_logger(__name__)
 def read_from_table(**kwargs):
   logger.debug("Read data from Hive with table name: " + kwargs["input"]["target"])
   sqlcontext = PysparkWrapper.init_processor(kwargs["id"])
+  kwargs["output"] = {"data": None}
   kwargs["output"]["data"] = sqlcontext.sql("select * from " + kwargs["input"]["target"])
   return kwargs
 # end def
